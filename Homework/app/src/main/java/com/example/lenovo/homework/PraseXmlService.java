df7ab160c057082c4fc29152f685bba7149d4a8d
Zhang <18883994559@163.com>
2015-06-02 21:43:30 +0800

    achieve update

--- Homework/app/src/main/java/com/example/lenovo/homework/PraseXmlService.java
+++ Homework/app/src/main/java/com/example/lenovo/homework/PraseXmlService.java
@@ -23,24 +23,17 @@ public class PraseXmlService
     {
         HashMap<String, String> hashMap = new HashMap<String, String>();
 
-        // 实例化一个文档构建器工厂
         DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
-        // 通过文档构建器工厂获取一个文档构建器
         DocumentBuilder builder = factory.newDocumentBuilder();
-        // 通过文档通过文档构建器构建一个文档实例
         Document document = builder.parse(inStream);
-        //获取XML文件根节点
         Element root = document.getDocumentElement();
-        //获得所有子节点
         NodeList childNodes = root.getChildNodes();
         for (int j = 0; j < childNodes.getLength(); j++)
         {
-            //遍历子节点
             Node childNode = (Node) childNodes.item(j);
             if (childNode.getNodeType() == Node.ELEMENT_NODE)
             {
                 Element childElement = (Element) childNode;
-                //版本号
                 if ("versionCode".equals(childElement.getNodeName())) {
                     hashMap.put("versionCode",childElement.getFirstChild().getNodeValue());
                 } else if (("versionName".equals(childElement.getNodeName()))) {
@@ -55,12 +48,6 @@ public class PraseXmlService
         return hashMap;
     }
 
-    /**
-     * 获取软件版本号
-     *
-     * @param context
-     * @return
-     */
     private int getVersionCode(Context context)
     {
         int versionCode = 0;
--- Homework/app/src/main/java/com/example/lenovo/homework/UpdateManager.java
+++ Homework/app/src/main/java/com/example/lenovo/homework/UpdateManager.java
@@ -26,29 +26,17 @@ import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.HashMap;
 
-/**
- *@author coolszy
- *@date 2012-4-26
- *@blog http://blog.92coding.com
- */
 
 public class UpdateManager
 {
-	/* 下载中 */
 	private static final int DOWNLOAD = 1;
-	/* 下载结束 */
 	private static final int DOWNLOAD_FINISH = 2;
-	/* 保存解析的XML信息 */
 	HashMap<String, String> mHashMap;
-	/* 下载保存路径 */
 	private String mSavePath;
-	/* 记录进度条数量 */
 	private int progress;
-	/* 是否取消更新 */
 	private boolean cancelUpdate = false;
 
 	private Context mContext;
-	/* 更新进度条 */
 	private ProgressBar mProgress;
 	private Dialog mDownloadDialog;
 
@@ -58,13 +46,10 @@ public class UpdateManager
 		{
 			switch (msg.what)
 			{
-			// 正在下载
 			case DOWNLOAD:
-				// 设置进度条位置
 				mProgress.setProgress(progress);
 				break;
 			case DOWNLOAD_FINISH:
-				// 安装文件
 				installApk();
 				break;
 			default:
@@ -78,15 +63,11 @@ public class UpdateManager
 		this.mContext = context;
 	}
 
-	/**
-	 * 检测软件更新
-	 */
 	public void checkUpdate()
 	{
 		if (isUpdate())
 		{
 			Toast.makeText(mContext,"客官请稍等",Toast.LENGTH_LONG).show();
-			// 显示提示对话框
 			showNoticeDialog();
 		} else
 		{
@@ -99,18 +80,10 @@ public class UpdateManager
 
 	InputStream inStream = PraseXmlService.class.getClassLoader().getResourceAsStream("http://hongyan.cqupt.edu.cn/app/cyxbsAppUpdate.xml");
 
-	/**
-	 * 检查软件是否有更新版本
-	 * 
-	 * @return
-	 */
 	private boolean isUpdate()
 	{
-		// 获取当前软件版本
 		int versionCode = getVersionCode(mContext);
-		// 把version.xml放到网络上，然后获取文件信息
 
-		// 解析XML文件。 由于XML文件比较小，因此使用DOM方式进行解析
 		PraseXmlService service = new PraseXmlService();
 		try
 		{
@@ -131,18 +104,11 @@ public class UpdateManager
 		return false;
 	}
 
-	/**
-	 * 获取软件版本号
-	 * 
-	 * @param context
-	 * @return
-	 */
 	private int getVersionCode(Context context)
 	{
 		int versionCode = 0;
 		try
 		{
-			// 获取软件版本号，对应AndroidManifest.xml下android:versionCode
 			versionCode = context.getPackageManager().getPackageInfo("com.szy.update", 0).versionCode;
 		} catch (NameNotFoundException e)
 		{
@@ -151,9 +117,6 @@ public class UpdateManager
 		return versionCode;
 	}
 
-	/**
-	 * 显示软件更新对话框
-	 */
 	private void showNoticeDialog()
 	{
 		// 构造对话框
@@ -191,52 +154,34 @@ public class UpdateManager
 		noticeDialog.show();
 	}
 
-	/**
-	 * 显示软件下载对话框
-	 */
 	private void showDownloadDialog()
 	{
-		// 构造软件下载对话框
 		Builder builder = new Builder(mContext);
 		builder.setTitle("正在更新");
-		// 给下载对话框增加进度条
 		final LayoutInflater inflater = LayoutInflater.from(mContext);
 		View v = inflater.inflate(R.layout.softupdate_progress, null);
 		mProgress = (ProgressBar) v.findViewById(R.id.update_progress);
 		builder.setView(v);
-		// 取消更新
 		builder.setNegativeButton("取消更新", new OnClickListener()
 		{
 			@Override
 			public void onClick(DialogInterface dialog, int which)
 			{
 				dialog.dismiss();
-				// 设置取消状态
 				cancelUpdate = true;
 			}
 		});
 		mDownloadDialog = builder.create();
 //		mDownloadDialog.show();
-		// 现在文件
 		downloadApk();
 	}
 
-	/**
-	 * 下载apk文件
-	 */
 	private void downloadApk()
 	{
 		// 启动新线程下载软件
 		new downloadApkThread().start();
 	}
 
-	/**
-	 * 下载文件线程
-	 * 
-	 * @author coolszy
-	 *@date 2012-4-26
-	 *@blog http://blog.92coding.com
-	 */
 	private class downloadApkThread extends Thread
 	{
 		@Override
@@ -244,23 +189,17 @@ public class UpdateManager
 		{
 			try
 			{
-				// 判断SD卡是否存在，并且是否具有读写权限
 				if (Environment.getExternalStorageState().equals(Environment.MEDIA_MOUNTED))
 				{
-					// 获得存储卡的路径
 					String sdpath = Environment.getExternalStorageDirectory() + "/";
 					mSavePath = sdpath + "download";
 					URL url = new URL("http://hongyan.cqupt.edu.cn/app/com.mredrock.cyxbs.apk");
-					// 创建连接
 					HttpURLConnection conn = (HttpURLConnection) url.openConnection();
 					conn.connect();
-					// 获取文件大小
 					int length = conn.getContentLength();
-					// 创建输入流
 					InputStream is = conn.getInputStream();
 
 					File file = new File(mSavePath);
-					// 判断文件目录是否存在
 					if (!file.exists())
 					{
 						file.mkdir();
@@ -270,18 +209,14 @@ public class UpdateManager
 					FileOutputStream fos = new FileOutputStream(apkFile);
 					Log.d("UpdateManager","222222222222");
 					int count = 0;
-					// 缓存
 					byte buf[] = new byte[1024];
 					Log.d("UpdateManager","33333333333");
-					// 写入到文件中
 					do
 					{
 						int numread = is.read(buf);
 						count += numread;
-						// 计算进度条位置
 						progress = (int) (((float) count / length) * 100);
 						Log.d("UpdateManager","4444444444");
-						// 更新进度
 						mHandler.sendEmptyMessage(DOWNLOAD);
 						if (numread <= 0)
 						{
@@ -290,9 +225,8 @@ public class UpdateManager
 							Log.d("UpdateManager", "55555555555");
 							break;
 						}
-						// 写入文件
 						fos.write(buf, 0, numread);
-					} while (!cancelUpdate);// 点击取消就停止下载.
+					} while (!cancelUpdate);
 					fos.close();
 					is.close();
 				}
@@ -303,14 +237,10 @@ public class UpdateManager
 			{
 				e.printStackTrace();
 			}
-			// 取消下载对话框显示
 			mDownloadDialog.dismiss();
 		}
 	};
 
-	/**
-	 * 安装APK文件
-	 */
 	private void installApk()
 	{
 		File apkfile = new File(mSavePath, "update_file");
@@ -318,7 +248,6 @@ public class UpdateManager
 		{
 			return;
 		}
-		// 通过Intent安装APK文件
 		Intent i = new Intent(Intent.ACTION_VIEW);
 		i.setDataAndType(Uri.parse("file://" + apkfile.toString()), "application/vnd.android.package-archive");
 		mContext.startActivity(i);
